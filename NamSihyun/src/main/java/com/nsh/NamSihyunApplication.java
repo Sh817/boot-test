8c492c22ff52a9133d402331b2c6abe32b545bc1
Nam <Nam@192.168.219.105>
2022-02-01 22:51:35 +0900

    test

---
+++ NamSihyun/src/main/java/com/nsh/NamSihyunApplication.java
@@ -0,0 +1,13 @@
+package com.nsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamSihyunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamSihyunApplication.class, args);
+	}
+
+}
